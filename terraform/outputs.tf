5cb98317c39d48fc734bbe771be63d901ae8918d
Josh Preston <joshuapreston@ucsb.edu>
2022-07-26 14:26:07 -0700

    created outputs for the user creation info

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,15 @@
 output "github_repository" {
     value                   = module.github_repository
 }
+
+output "aws_iam_group_policy" {
+    value                   = aws_iam_group_policy.iam_group_policy
+}
+
+output "aws_iam_user" {
+    value = {
+      username              = aws_iam_user.veeam
+      acceess_key_id        = aws_iam_access_key.veeam.id
+      secret_access_key_id  = aws_iam_access_key.veeam.secret 
+    }
+}
